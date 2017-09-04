bc22b13e9f4a400048b4d805191982a9d4bdfb04
Ionut Costica <ionut.costica@gmail.com>
2017-09-04 04:31:08 +0300

    feat: add interceptors to `createHttpClient`

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import { HttpClient } from './src/common-http';
 import { Provider, ReflectiveInjector } from 'injection-js';
 import { HttpXhrBackend } from './src/common-http/src/xhr';
 import { HttpBackend } from './src/common-http/src/backend';
+import { Type } from 'injection-js/facade/type';
+import { HTTP_INTERCEPTORS } from './src/common-http/src/interceptor';
 
 require('zone.js');
 
@@ -13,8 +15,21 @@ export const HTTP_CLIENT_PROVIDERS: Provider[] = [
 	{provide: HttpBackend, useExisting: HttpXhrBackend},
 ];
 
-export function createHttpClient(): HttpClient {
-	return ReflectiveInjector.resolveAndCreate(HTTP_CLIENT_PROVIDERS).get(HttpClient);
+export interface CreateHttpClientOptions {
+	interceptors?: Type<any>[];
+}
+
+export function createHttpClient(options: CreateHttpClientOptions = {}): HttpClient {
+	const providers = [
+		...HTTP_CLIENT_PROVIDERS,
+		...(options.interceptors
+			? options.interceptors.map(interceptor =>
+				({ provide: HTTP_INTERCEPTORS, useClass: interceptor, multi: true })
+			)
+			: []
+		)
+	];
+	return ReflectiveInjector.resolveAndCreate(providers).get(HttpClient);
 }
 
 export * from './src/http';
